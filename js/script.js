e2d827ba1ad497aa0407bd5cb8f3e650ecc085f7
Brandon Johnson <johnsbr99@gmail.com>
2023-10-10 19:33:46 -0600

    Added GH API and populated user info

--- js/script.js
+++ js/script.js
@@ -1,2 +1,31 @@
 const profileOverview=document.querySelector("div.overview");
-const username=johnson-brandon;
+const username="johnson-brandon";
+
+const getData=async function () {
+    const userInfo = await fetch(
+        `https://api.github.com/users/${username}`
+    );
+    const data = await userInfo.json();
+    console.log(data);
+    displayUserInfo(data);
+};
+
+getData();
+
+const displayUserInfo = function (data) {
+    const div = document.createElement("div");
+    div.classList.add("user-info");
+    div.innerHTML=`
+        <figure>
+        <img alt="user avatar" src=${data.avatar_url} />
+        </figure>
+        <div>
+        <p><strong>Name:</strong> ${data.name}</p>
+        <p><strong>Bio:</strong> ${data.bio}</p>
+        <p><strong>Location:</strong> ${data.location}</p>
+        <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+        </div>
+        `;
+    profileOverview.append(div);
+};
+
