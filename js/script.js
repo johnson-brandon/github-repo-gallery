276adfc49d45c29b7e23703d7990103005398444
Brandon Johnson <johnsbr99@gmail.com>
2023-10-12 07:46:34 -0600

    Fetch repo data & display on page

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 const profileOverview=document.querySelector("div.overview");
 const username="johnson-brandon";
+const repoList=document.querySelector("ul.repo-list");
 
+// Collects user info from GitHub
 const getData=async function () {
     const userInfo = await fetch(
         `https://api.github.com/users/${username}`
     );
     const data = await userInfo.json();
-    console.log(data);
+    // console.log(data);
     displayUserInfo(data);
 };
-
 getData();
 
+//Displays user profile data on page
 const displayUserInfo = function (data) {
     const div = document.createElement("div");
     div.classList.add("user-info");
@@ -27,5 +29,24 @@ const displayUserInfo = function (data) {
         </div>
         `;
     profileOverview.append(div);
+    fetchRepos();
 };
 
+// Collects repo data sorted by last updated and limits to 100 per page
+const fetchRepos = async function () {
+    const listRepos = await fetch (
+        `https://api.github.com/users/${username}/repos?sort=updated&per_page=100`
+    );
+    const repoData = await listRepos.json();
+    displayRepos(repoData);
+};
+
+// Displays repo data on page
+const displayRepos = function (repos) {
+    for (repo of repos) {
+        const repoItem=document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
+}
