c302a26050cdd867a05a9ea6b571c01737eb856d
Brandon Johnson <johnsbr99@gmail.com>
2023-10-18 07:48:13 -0600

    h3 li toUpperCase

--- js/script.js
+++ js/script.js
@@ -52,7 +52,7 @@ const displayRepos = function (repos) {
     for (const repo of repos) {
         const repoItem=document.createElement("li");
         repoItem.classList.add("repo");
-        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoItem.innerHTML = `<h3>${repo.name.toUpperCase()}</h3>`;
         repoList.append(repoItem);
     }
 };
